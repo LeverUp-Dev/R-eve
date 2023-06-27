0d4fc002496067811b989ab2b231bf7baece8f1d
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2023-06-27 15:13:55 +0900

    Delegate&Lambda1

---
+++ 방학 1주차/Delegate&Lambda/Delegate&Lambda1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Test
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+    internal class Program
+    {
+        
+        private static void Main(string[] args)
+        {
+            //1.메서드의 매개변수로 메서드를 전달하는 방법
+            List<Product> products = new List<Product>()
+            {
+                new Product() {Name = "감자", Price = 1000},
+                new Product() {Name = "사과", Price = 1200},
+                new Product() {Name = "고구마", Price = 700},
+                new Product() {Name = "배추", Price = 900}
+            };
+
+            products.Sort(SortWithPrice);
+
+            foreach (Product item in products)
+            {
+                Console.WriteLine(item.Name + ":" + item.Price + "원");
+            }
+        }
+
+        private static int SortWithPrice(Product x, Product y)
+        {
+            return x.Price .CompareTo(y.Price);
+        }
+    }
+}
---
+++ 방학 1주차/Delegate&Lambda/Delegate&Lambda1_2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Test
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+    internal class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            //2.무명 델리게이트 방식
+            List<Product> products = new List<Product>()
+            {
+                new Product() {Name = "감자", Price = 1000},
+                new Product() {Name = "사과", Price = 1200},
+                new Product() {Name = "고구마", Price = 700},
+                new Product() {Name = "배추", Price = 900}
+            };
+
+            products.Sort(delegate(Product a, Product b) // 또는 람다식 방식: (a, b) =>
+            {
+                return a.Name.CompareTo(b.Name);
+            });
+
+            foreach (Product item in products)
+            {
+                Console.WriteLine(item.Name + ":" + item.Price + "원");
+            }
+        }
+    }
+}
