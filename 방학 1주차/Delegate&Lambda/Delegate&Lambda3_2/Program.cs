6178f0881785dc97dd40cbc91b2b8764b1be0dd0
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2023-06-27 20:23:56 +0900

    예외처리 방학1주차 과제 마지막

--- 방학 1주차/Delegate&Lambda/Delegate&Lambda3_2/Program.cs
+++ 방학 1주차/Delegate&Lambda/Delegate&Lambda3_2/Program.cs
@@ -4,7 +4,13 @@
     {
         private static void Main(string[] args)
         {
+            string[] array = { "가", "나", "다" };
+            int input = int.Parse(Console.ReadLine());
 
+            if (input < array.Length)
+                Console.WriteLine("입력위치는 " + array[input] + " 입니다.");
+            else
+                Console.WriteLine("인덱스 범위를 벗어났습니다.");
         }
     }
 }
