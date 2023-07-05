1b9ab86f09fc4305aef13dd436319d38b5638f42
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2023-07-05 17:04:00 +0900

    방학 3주차

---
+++ R-eve/Assets/Scenes/HanoiManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HanoiManager : MonoBehaviour
+{
+    public GameObject disks;
+    public Transform towersTr;
+    public Transform spawnPoint;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
